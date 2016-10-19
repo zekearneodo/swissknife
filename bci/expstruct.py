4a0e1818795e2959cdeb892a7a0d8c328e8050aa
Zeke Arneodo <earneodo@ucsd.edu>
2016-10-19 14:03:51 -0700

    updates to experiment structure

--- bci/expstruct.py
+++ bci/expstruct.py
@@ -106,10 +106,12 @@ def file_path(fn_dict, folder_key, file_key):
 
 
 def mkdir_p(path):
+    logger.debug('Creating directory {}'.format(path))
     try:
         os.makedirs(path)
     except OSError as exc:  # Python >2.5
         if exc.errno == errno.EEXIST and os.path.isdir(path):
+            logger.debug('Directory existed, doing nothing')
             pass
         else:
             raise
